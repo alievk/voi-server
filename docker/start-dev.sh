88eb639c261267add2370fe5ae904f709c7bfdb4
Ali Aliev <ali@avatarify.ai>
2025-02-07 09:27:36 +0000

    fix paths&names

--- docker/start-dev.sh
+++ docker/start-dev.sh
@@ -2,4 +2,4 @@
 
 sudo /usr/sbin/sshd -D &
 
-jupyter-lab --ip=0.0.0.0 --port=8500 --no-browser --notebook-dir=/home/user/sources
+jupyter-lab --ip=0.0.0.0 --port=8500 --no-browser --notebook-dir=/home/user
