0b92df1e27be1c0b32912d25ece51dd630e048ae
Ali Aliev <ali@avatarify.ai>
2025-02-07 15:50:20 +0000

    jupyter won't run without a token

--- docker/start-dev.sh
+++ docker/start-dev.sh
@@ -2,4 +2,9 @@
 
 sudo /usr/sbin/sshd -D &
 
-jupyter-lab --ip=0.0.0.0 --port=8500 --no-browser --notebook-dir=/home/user
+if [ -z "${JUPYTER_TOKEN}" ]; then
+    echo "JUPYTER_TOKEN environment variable is not set"
+    tail -f /dev/null
+else
+    jupyter-lab --ip=0.0.0.0 --port=8500 --no-browser --notebook-dir=/home/user --ServerApp.token=${JUPYTER_TOKEN}
+fi
